3e36f5348b6401479437424b3595ffc2792a0d87
Pj <98929418+praveendevops1337@users.noreply.github.com>
2022-04-13 22:46:40 +0530

    Delete s3.tf

--- s3.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "pjbucketjhz" {
-  bucket = "my-tf-massiv969eprince-bucket11"
-  acl    = "private"
-
-  tags = {
-    Name        = "rav1i-day1a-santhos1h-deepak1231"
-    Environment = "Dev"
-  }
-}
