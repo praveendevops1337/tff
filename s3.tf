8953e4be5031f2aa766258c5a485511a89985556
Pj <98929418+praveendevops1337@users.noreply.github.com>
2022-04-13 23:11:34 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
-resource "aws_s3_bucket" "hd11pmbucket" {
-  bucket = "my-tf-hd11pmbucket-bucket11"
+resource "aws_s3_bucket" "hd11pmbuckeet" {
+  bucket = "my-tf-hd11pmbuckeet-bucket11"
 
   tags = {
     Name        = "rav1i-day1a-santhos1h-deepak1231"
