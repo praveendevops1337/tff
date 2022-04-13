65897a919e8cb268aee842f4ce787baec6178e37
Pj <98929418+praveendevops1337@users.noreply.github.com>
2022-04-13 22:54:04 +0530

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,8 @@
+resource "aws_s3_bucket" "bucklebuck" {
+  bucket = "my-tf-massiveprince-bucket11"
+
+  tags = {
+    Name        = "rav1i-day1a-santhos1h-deepak1231"
+    Environment = "Dev"
+  }
+}
